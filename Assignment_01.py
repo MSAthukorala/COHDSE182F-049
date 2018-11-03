3e9f186c964782b8a1f658e4e992bcfc870ba70c
MSAthukorala <39034270+MSAthukorala@users.noreply.github.com>
2018-11-03 23:17:02 +0530

    Add files via upload

---
+++ Assignment_01.py
@@ -0,0 +1,40 @@
+'''
+Created on Nov 3, 2018
+
+@author: HP
+'''
+key = 'abcdefghijklmnopqrstuvwxyz'
+
+def encrypt(n, plaintext):
+    """Encrypt the string and return the ciphertext"""
+    result = ''
+
+    for l in plaintext.lower():
+        try:
+            i = (key.index(l) + n) % 26
+            result += key[i]
+        except ValueError:
+            result += l
+
+    return result.lower()
+
+def decrypt(n, ciphertext):
+    result = ''
+
+    for l in ciphertext:
+        try:
+            i = (key.index(l) - n) % 26
+            result += key[i]
+        except ValueError:
+            result += l
+
+    return result
+
+text = "hello"
+offset = 1
+
+encrypted = encrypt(offset, text)
+print('Encrypted:', encrypted)
+
+decrypted = decrypt(offset, encrypted)
+print('Decrypted:', decrypted)
